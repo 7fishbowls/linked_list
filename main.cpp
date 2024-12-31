39d0694662f7ba4812b90ae6bbf2ee5cba0dbc5b
Shaik Ali <7fishbowls@gmail.com>
2024-12-31 09:57:57 -0800

    Basic Linked List Implemented

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ class Node{
     int data;
     Node * nextNode;
 
+    public:
     Node(int data){
         this->data = data;
         nextNode = nullptr;
@@ -14,6 +15,10 @@ class Node{
         nextNode = node;
     }
 
+    Node* getNext(){
+        return nextNode;
+    }
+
     int getData(){
         return data;
     }
@@ -21,10 +26,67 @@ class Node{
 };
 
 class List{
+
     Node * head = nullptr;
     Node * tail = nullptr;
+
+    public:
+
+        void addNode(int data){
+
+            Node * newNode = new Node(data);
+
+            if(head == nullptr){
+                head = tail = newNode;
+            }
+            else {
+                newNode->setNode(head);
+                head = newNode;
+            }
+        }
+
+        void pushNode(int data){
+            Node * newNode = new Node(data);
+
+            if(head == nullptr){
+                head = tail = newNode;
+                return;
+            }
+
+            tail->setNode(newNode);
+            tail = newNode;
+
+        }
+
+        void printNodes(){
+            Node * temp = head;
+            while(temp != nullptr){
+
+                cout << temp->getData() << " -> ";
+                temp = temp->getNext();
+
+            }
+        }
+
+        void getHead(){
+            cout << "\nHead: " << head->getData();
+        }
+
+        void getTail(){
+            cout << "\nTail: " << tail->getData();
+        }
+
 };
 
 int main(){
+    List list;
+
+    for(int i = 0; i < 10000; i++){
+        list.pushNode(i);
+    }
+
+    list.printNodes();
+    list.getHead();
+    list.getTail();
     return 0;
 }
